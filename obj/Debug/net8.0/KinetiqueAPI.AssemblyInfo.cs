5d9662441d02b32cb8257962a7f550582434f498
see-simon <sea6580@gmail.com>
2026-07-15 15:29:32 +0200

    Cleaned up project and removed credentials

--- obj/Debug/net8.0/KinetiqueAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/KinetiqueAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KinetiqueAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a310bbd51513cddd50bf973100b57bb907ac8740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356ce28350650945eb272cb3d443eb3ffc96b601")]
 [assembly: System.Reflection.AssemblyProductAttribute("KinetiqueAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KinetiqueAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
